2166b6fff01aadd751dacfe147ca76ccc3837395
Afraz Momin <41325430+afrazchelsea@users.noreply.github.com>
2020-05-27 20:08:15 +0530

    vowels

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,24 +14,22 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n, row = 0, level = "") {
-  if (row === n) {
-    return;
-  }
-
-  if (level.length === 2 * n - 1) {
-    console.log(level);
-    return pyramid(n, row + 1);
-  }
-
+function pyramid(n) {
   const midpoint = Math.floor((2 * n - 1) / 2);
-  let add;
-  if (midpoint - row <= level.length && midpoint + row >= level.length) {
-    add = "#";
-  } else {
-    add = " ";
+
+  for (let row = 0; row < n; row++) {
+    let level = "";
+
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+
+    console.log(level);
   }
-  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
